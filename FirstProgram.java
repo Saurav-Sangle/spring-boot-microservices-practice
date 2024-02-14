a9d15a67b0db0c594c2919d8150907560f2cbdaf
Saurav-Sangle <sauravsangle2021@gmail.com>
2024-02-14 16:04:01 +0530

    FirstProgram

---
+++ FirstProgram.java
@@ -0,0 +1,5 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Wish You All The Best For Your Journey to Master on SpringBoot Microservices !");
+    }
+}
---
+++ out/production/spring-boot-microservices-practice/README.md
@@ -0,0 +1,2 @@
+# spring-boot-microservices-practice
+Explore my passion for Spring Boot microservices! Dive into practical examples showcasing dedication. Perfect for enthusiasts seeking inspiration.
